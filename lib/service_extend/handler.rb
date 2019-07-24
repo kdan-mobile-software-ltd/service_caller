06d43d78ed9a500eac6f8a73add41d21bfa37688
JiaRou <laura34963@kdanmobile.com>
2019-07-24 15:27:04 +0800

    remove rescue record not found error in handler module

--- lib/service_extend/handler.rb
+++ lib/service_extend/handler.rb
@@ -4,9 +4,6 @@ module ServiceExtend
       super
     rescue ServiceError => e
       @error = e
-    rescue ActiveRecord::RecordNotFound => e
-      @error = ServiceError.new(404, e.message)
-      @error.set_backtrace(e.backtrace)
     rescue Exception => e
       @error = ServiceError.generate_from_exception(e)
     end
