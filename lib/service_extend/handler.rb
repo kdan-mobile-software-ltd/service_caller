661af742733f25ad51651eaec40a89c26e63da74
JiaRou <laura34963@kdanmobile.com>
2019-08-01 11:21:41 +0800

    add handler for rails engine

--- lib/service_extend/handler.rb
+++ lib/service_extend/handler.rb
@@ -1,9 +1,26 @@
 module ServiceExtend
   module Handler
     def call
+      defined?(::Rails::Engine) ? rails_call : ruby_call
+    end
+
+    private
+
+    def ruby_call
       super
     rescue ServiceError => e
       @error = e
+    rescue Exception => e
+      @error = ServiceError.generate_from_exception(e)
+    end
+
+    def rails_call
+      super
+    rescue ServiceError => e
+      @error = e
+    rescue ActiveRecord::RecordNotFound => e
+      @error = ServiceError.new(404, e.message)
+      @error.set_backtrace(e.backtrace)
     rescue Exception => e
       @error = ServiceError.generate_from_exception(e)
     end
