4eb8affb879ec1e57afc212535db4361fac76a79
JiaRou <laura34963@kdanmobile.com>
2019-08-01 11:37:59 +0800

    fix handler bug

--- lib/service_extend/handler.rb
+++ lib/service_extend/handler.rb
@@ -1,28 +1,28 @@
 module ServiceExtend
   module Handler
+
     def call
-      defined?(::Rails::Engine) ? rails_call : ruby_call
+      if defined?(::Rails::Engine)
+        begin
+          super
+        rescue ServiceError => e
+          @error = e
+        rescue ActiveRecord::RecordNotFound => e
+          @error = ServiceError.new(404, e.message)
+          @error.set_backtrace(e.backtrace)
+        rescue Exception => e
+          @error = ServiceError.generate_from_exception(e)
+        end
+      else
+        begin
+          super
+        rescue ServiceError => e
+          @error = e
+        rescue Exception => e
+          @error = ServiceError.generate_from_exception(e)
+        end
+      end
     end
 
-    private
-
-    def ruby_call
-      super
-    rescue ServiceError => e
-      @error = e
-    rescue Exception => e
-      @error = ServiceError.generate_from_exception(e)
-    end
-
-    def rails_call
-      super
-    rescue ServiceError => e
-      @error = e
-    rescue ActiveRecord::RecordNotFound => e
-      @error = ServiceError.new(404, e.message)
-      @error.set_backtrace(e.backtrace)
-    rescue Exception => e
-      @error = ServiceError.generate_from_exception(e)
-    end
   end
 end
