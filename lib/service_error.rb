da588e1dd7f4192ca1caafc874912f46b774c235
JiaRou <laura34963@gmail.com>
2019-09-16 16:18:34 +0800

    service error can assign error_obj

--- lib/service_error.rb
+++ lib/service_error.rb
@@ -1,16 +1,18 @@
 class ServiceError < StandardError
   attr_reader :key
+  attr_reader :error_obj
 
   class << self
     def generate_from_exception(exception)
-      service_err = new(:internal_error, "#{exception.class.name}: #{exception.message}")
+      service_err = new(:internal_error, error_msg: "#{exception.class.name}: #{exception.message}")
       service_err.set_backtrace(exception.backtrace)
       service_err
     end
   end
 
-  def initialize(key, message=nil)
+  def initialize(key, error_obj={})
     @key = key
-    super(message || key)
+    @error_obj = error_obj
+    super(error_obj[:error_msg] || key)
   end
 end
